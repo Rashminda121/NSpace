c7021afef3fea17647885d47f81453cab24cb9ba
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-03-07 18:05:37 +0530

    added signin page

---
+++ signin.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/0.7.5/flowbite.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <title>Hostel Management System</title>
+</head>
+<body>
+<?php include("Navbar.php"); ?>
+    <div style="background-image: url('https://www.nsbm.ac.lk/wp-content/uploads/2021/08/About-Tab-1.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat;" class="min-h-screen flex flex-col justify-center py-8 bg-gray-700">
+
+        <div class="bg-white p-8 md:p-12 rounded-lg shadow-lg mx-auto max-w-md md:max-w-4xl m-20">
+    <h1 class="mb-4 text-4xl font-bold tracking-tight leading-none text-black md:text-2xl lg:text-3xl text-center">Efficiency Meets Comfort: Streamlining Hostel Life at NSBM Green University!</h1>
+    <p class="mb-8 text-lg font-bold text-gray-800 lg:text-xl sm:px-16 lg:px-32 text-center">Login As:</p>
+    <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+        <button class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 ml-3">
+            Landlord 
+            <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180 ml-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+        </button>
+        <button class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 ml-3">
+            Warden
+            <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180 ml-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+        </button>
+        <button class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 ml-3">
+            Student
+            <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180 ml-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+        </button>
+        <button class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 ml-3">
+            Admin
+            <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180 ml-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+        </button>
+    </div>
+</div>
+</div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/0.7.5/flowbite.min.js"></script>
+</body>
+</html>
