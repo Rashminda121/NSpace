22d86430e4f3b1e7d5733c6d0ec1b2815e953c18
avish madushanka <avishmadushanka21@gmail.com>
2024-03-08 22:39:54 +0530

    update card

--- student/home.php
+++ student/home.php
@@ -53,7 +53,7 @@
                     alt="Product" class="h-60 w-72 object-cover rounded-t-xl" />
             <div class="px-4 py-3 w-72">
             <p class="text-lg font-bold text-black truncate block capitalize">How to manage hostel life</p>
-                <span class="text-gray-400 mr-3 uppercase text-xs">2024-03-08</span>
+            <span class="text-gray-400 mr-3 uppercase text-xs">2024-03-08</span>
             </div>
         </a>
     </div>
@@ -62,12 +62,11 @@
     <!-- Product card 3 -->
     <div class="h-80 w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
         <a href="#">
-            <img src="https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            <img src="image/6.jpg"
                     alt="Product" class="h-60 w-72 object-cover rounded-t-xl" />
             <div class="px-4 py-3 w-72">
-                <span class="text-gray-400 mr-3 uppercase text-xs">Brand</span>
-                <p class="text-lg font-bold text-black truncate block capitalize">Product Name</p>
-            </div>
+            <p class="text-lg font-bold text-black truncate block capitalize">How to manage hostel life</p>
+            <span class="text-gray-400 mr-3 uppercase text-xs">2024-03-08</span>
         </a>
     </div>
     <!-- Product card 3 Ends-->
