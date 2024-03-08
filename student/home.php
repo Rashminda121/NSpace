b7a7e7ce8c85216ea660b25464ef469654de3d9c
avish madushanka <avishmadushanka21@gmail.com>
2024-03-08 22:53:23 +0530

    create article page

--- student/home.php
+++ student/home.php
@@ -29,7 +29,7 @@
             </div>
         </div>
     </section>
-
+    <br>
     <section id="Projects"
     class="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
 
