30b523b45faaf64ac4362df35340cd57eaeb515a
avish madushanka <avishmadushanka21@gmail.com>
2024-03-09 00:46:33 +0530

    update articles page

--- student/articles.php
+++ student/articles.php
@@ -5,7 +5,12 @@
 
 <body>
 <?php include ("Navbar.php"); ?>
-
+<div class="home">
+        <div class="home_background parallax-window" data-parallax="scroll" data-image-src="image/4.jpg"></div>
+        <div class="home_content">
+            <div class="home_title">About Us</div>
+        </div>
+    </div>
 
 </body>
 </html>
