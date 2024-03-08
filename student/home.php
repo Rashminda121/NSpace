a505e65a563e4eaee647d2663d73246b47ac66d4
avish madushanka <avishmadushanka21@gmail.com>
2024-03-08 16:06:27 +0530

    update student home page

--- student/home.php
+++ student/home.php
@@ -3,7 +3,8 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
 </head>
 <body>
-    
+    <?php include ("Navbar.php"); ?>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
 </body>
 </html>
