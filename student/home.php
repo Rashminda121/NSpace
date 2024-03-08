bee2616e8c32af471e214b75cf0fa7c02a5b9af9
avish madushanka <avishmadushanka21@gmail.com>
2024-03-08 17:32:47 +0530

    update student home page

--- student/home.php
+++ student/home.php
@@ -10,13 +10,13 @@
         <div class="mr-auto place-self-center lg:col-span-7">
             <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">Welcome to our hostel Management System!</h1>
             <p class="max-w-2xl mb-6 font-medium text-yellow-300 lg:mb-8 md:text-lg lg:text-xl ">Where strangers become friends and adventures begin</p>
-            <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-primary-300">
+            <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg bg-[#7dd3fc] hover:bg-[#0369a1] focus:ring-4 focus:ring-gray-100">
+                Speak to Sales
+            </a> 
+            <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white border border-gray-300 rounded-lg bg-[#0369a1] hover:bg-[#7dd3fc] focus:ring-4 focus:ring-primary-300">
                 Get started
                 <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a>
-            <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100">
-                Speak to Sales
-            </a> 
         </div>
         <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
             <img src="image/2.avif" alt="mockup" style="width: 800px; height: 300px;">
