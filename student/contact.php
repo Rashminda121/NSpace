17223940ea73c33698c4ad5ccf0e6c764d98f439
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-03-10 00:37:18 +0530

    created contact page frontend

--- student/contact.php
+++ student/contact.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 <?php include ("Navbar.php"); ?>
-<section class="bg-white dark:bg-gray-900">
+<section class="bg-white dark:bg-gray-900 mt-16">
 
   <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
       <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
