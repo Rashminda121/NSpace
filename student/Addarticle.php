877751d1fe23df14983cc2e78b5b7f0f90df1bb7
avish madushanka <avishmadushanka21@gmail.com>
2024-03-09 22:30:01 +0530

    article form updated

--- student/Addarticle.php
+++ student/Addarticle.php
@@ -8,10 +8,11 @@
 
 <body>
     <?php include ("Navbar.php"); ?>
-
+    
     <section class="bg-white dark:bg-gray-900">
   <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
-      <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Add a new product</h2>
+      <h2 class="mb-4 text-2xl font-bold text-gray-900 dark:text-white"><center>Add a New Article</center></h2>
+      <br>
       <form action="#">
           <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
               <div class="sm:col-span-2">
