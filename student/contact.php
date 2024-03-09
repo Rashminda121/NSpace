8f449e7df8405bd5a013f84e7e32ec3f8499bdc4
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-03-09 23:55:31 +0530

    modified

--- student/contact.php
+++ student/contact.php
@@ -6,8 +6,9 @@
     <title>Hostel Management System</title>
 </head>
 <body>
-
+<?php include ("Navbar.php"); ?>
 <section class="bg-white dark:bg-gray-900">
+
   <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
       <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
       <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about our Hostel Management System? Please Let us know.</p>
