43f34904d50a0852ba8dd44aa9dba451a6de50b3
Onaliy Vinukiy Jayawardana <ovjayawardana@students.nsbm.ac.lk>
2024-03-09 17:26:46 +0530

    Update dbConfig.php

--- landlord/dbConfig.php
+++ landlord/dbConfig.php
@@ -1,19 +1,5 @@
 <?php
-<<<<<<< HEAD
-function OpenCon()
-{
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "avi";
-$dbname = "nspacedb";
-$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die("Connect failed: %s\n". $conn -> error);
-return $conn;
-}
-function CloseCon($conn)
-{
-$conn -> close();
-}
-=======
+
 function dbCon()
 {
     $dbhost = "localhost";
@@ -28,4 +14,3 @@ function conClose($conn)
     $conn->close();
 }
 ?>
->>>>>>> 8ba45b023ef5bb434ddea77e7baeafddf7c707d2
