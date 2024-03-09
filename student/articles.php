12ceb3fd64e8cc47c0ceae2b8184c2f48257b4ff
avish madushanka <avishmadushanka21@gmail.com>
2024-03-09 20:21:25 +0530

    update article page mobile view

--- student/articles.php
+++ student/articles.php
@@ -1,10 +1,11 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hostel Management System</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/0.7.5/flowbite.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-white dark:bg-gray-900">
     <?php include ("Navbar.php"); ?>
@@ -51,5 +52,7 @@
             </article>                  
         </div>  
     </section>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>  
 </body>
 </html>
