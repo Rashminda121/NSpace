3888468972b54d7f1aa31ab3b439b48dd4e24767
avish madushanka <avishmadushanka21@gmail.com>
2024-03-11 19:53:32 +0530

    update edit article page

---
+++ admin/editArticle.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/0.7.5/flowbite.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Add Article</title>
+</head>
+<body>
+<?php include("adminNavbar.php"); ?>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+</body>
+</html>
