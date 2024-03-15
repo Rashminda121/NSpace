7219b48222dce7f8d5fc1ab725aac9c077ceb7d7
avish madushanka <avishmadushanka21@gmail.com>
2024-03-15 17:02:29 +0530

    update DB code

--- admin/editStudent.php
+++ admin/editStudent.php
@@ -3,7 +3,7 @@
 require_once("dbConfig.php");
 
 $conn = OpenCon();
-$sql = "SELECT * FROM addStudent";
+$sql = "SELECT * FROM student";
 $result = $conn->query($sql);
 
 $article = [];
@@ -21,7 +21,7 @@ CloseCon($conn);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NSPACE - Hostel Management System</title>
+   55
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
