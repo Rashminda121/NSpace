a6dcdb81eaba1c2e1b45a2f1441465222394934e
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-03-16 12:31:57 +0530

    completed feedback delete function

--- admin/feedbackEdit.php
+++ admin/feedbackEdit.php
@@ -14,7 +14,7 @@ if ($result->num_rows > 0) {
 }
 // Delete Action
 if (isset($_POST['delete'])) {
-    $feedback_id = $_POST['delete']; // Get the id of the feedback to delete
+    $feedback_id = $_POST['feedback_id']; 
     $sql = "DELETE FROM feedback WHERE id=?";
     $stmt = mysqli_prepare($conn, $sql);
     if ($stmt) {
@@ -97,10 +97,12 @@ CloseCon($conn);
             <a href="edit_Feedback.php?id=<?php echo $feedback['id']; ?>" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
         </td>
         <td class="px-6 py-4">
-        <form action="" method="POST" onsubmit="return confirmDelete()">
-            <button type="submit" name="delete"
-            class="font-medium text-red-600 dark:text-red-500 hover:underline mt-3">Delete</button>
-        </form>
+            <form action="" method="POST" onsubmit="return confirmDelete()">
+                <input type="hidden" name="feedback_id" value="<?php echo $feedback['id']; ?>">
+                    <button type="submit" name="delete" value="<?php echo $feedback['id']; ?>"
+                    class="font-medium text-red-600 dark:text-red-500 hover:underline mt-3">Delete</button>
+            </form>
+
         </td>
        
     </tr>
